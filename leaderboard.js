9d17fa132cfd7125a1166740d90603182085efd2
thomas <thomashuan88@gmail.com>
2016-03-10 01:51:37 +0800

    chapter 2 - sorted set

--- leaderboard.js
+++ leaderboard.js
@@ -36,3 +36,51 @@ LeaderBoard.prototype.showTopUsers = function(quantity) {
 		}
 	});
 };
+
+LeaderBoard.prototype.getUsersAroundUser = function(username, quantity, callback) {
+	var leaderboardKey = this.key;
+	client.zrevrank(leaderboardKey, username, function(err, zrevrankReply) {
+		var startOffset = Math.floor(zrevrankReply - (quantity / 2) + 1);
+
+		if (startOffset < 0) {
+			startOffset = 0;
+		}
+		var endOffset = startOffset + quantity - 1;
+
+		client.zrevrange([leaderboardKey, startOffset, endOffset, "WITHSCORES"], function(err, zrevrangeReply) {
+			var users = [];
+			for (var i = 0, rank = 1; i < zrevrangeReply.length; i += 2, rank++) {
+				var user = {
+					rank: startOffset + rank,
+					score: zrevrangeReply[i + 1],
+					username: zrevrangeReply[i]
+				};
+				users.push(user);
+			}
+			callback(users);
+		});
+	});
+};
+
+var leaderBoard = new LeaderBoard("game-score");
+leaderBoard.addUser("Arthur", 70);
+leaderBoard.addUser("KC", 20);
+leaderBoard.addUser("Maxwell", 10);
+leaderBoard.addUser("Patrik", 30);
+leaderBoard.addUser("Ana", 60);
+leaderBoard.addUser("Felipe", 40);
+leaderBoard.addUser("Renata", 50);
+leaderBoard.addUser("Hugo", 80);
+leaderBoard.removeUser("Arthur");
+
+leaderBoard.getUserScoreAndRank("Maxwell");
+
+leaderBoard.showTopUsers(3);
+
+leaderBoard.getUsersAroundUser("Felipe", 5, function(users) {
+	console.log("\nUsers around Felipe:");
+	users.forEach(function(user) {
+		console.log("#" + user.rank, "User:", user.username + ", score:", user.score);
+	});
+	client.quit();
+});
